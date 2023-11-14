3b4fd101713ca07d19ddb65a8a189ac7eb1ced78
datsfilipe <datsfilipe.foss@proton.me>
2023-11-14 12:38:12 -0300

    feat: add commands working flow on main

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,80 @@ mod list;
 mod commands;
 
 fn main() {
-    let mut _manager = manager::Manager::new();
+    let mut manager = manager::Manager::new();
 
     let args: Vec<String> = std::env::args().collect();
     if args.len() == 1 {
         commands::help();
-        return;
     }
 
     let command = &args[1];
     if command == "help" {
         commands::help();
-        return;
     }
+
+    if command == "add" {
+        if args.len() == 3 {
+            commands::add(&mut manager, &args[2]);
+        }
+        if args.len() == 4 {
+            commands::add_item(&mut manager, &args[2], &args[3]);
+        }
+    }
+
+    if command == "remove" {
+        if args.len() == 3 {
+            commands::remove(&mut manager, &args[2]);
+        }
+        if args.len() == 4 {
+            commands::remove_item(&mut manager, &args[2], &args[3]);
+        }
+    }
+
+    if command == "list" {
+        if args.len() == 2 {
+            let lists = commands::list(&manager);
+            for list in lists {
+                println!("{}", list);
+            }
+        }
+        if args.len() == 3 {
+            let list = manager.get_list(&args[2]).unwrap();
+            for item in list.get_list() {
+                println!("{}", item.get_content());
+            }
+        }
+        if args.len() == 4 {
+            let status = match &args[3][..] {
+                "todo" => list::Status::Todo,
+                "doing" => list::Status::Doing,
+                "done" => list::Status::Done,
+                _ => panic!("Invalid status"),
+            };
+            commands::get_list_by_status(&manager, &args[2], status);
+        }
+    }
+
+    if command == "edit" {
+        if args.len() == 4 {
+            commands::edit_list(&mut manager, &args[2], &args[3]);
+        }
+        if args.len() == 5 {
+            commands::edit_item(&mut manager, &args[2], &args[3], &args[4]);
+        }
+    }  
+
+    if command == "get" {
+        if args.len() == 4 {
+            let item = commands::get_item(&manager, &args[2], &args[3]).unwrap();
+            println!("{}", item);
+        }
+    }
+
+    let lists = commands::list(&manager);
+    for list in lists {
+        println!("{}", list);
+    }
+
+    commands::help();
 }
