e0bf5cef8dde926603f9481e8ddbfcd42a6b8d72
datsfilipe <datsfilipe.foss@proton.me>
2023-11-14 17:41:33 -0300

    refactor: adding and updating commands

--- src/commands.rs
+++ src/commands.rs
@@ -11,8 +11,9 @@ pub fn help () {
     println!("  list [list] [status] - List all items in a list by status");
     println!("  add [list] [item] - Add an item to a list");
     println!("  remove [list] [item] - Remove an item from a list");
-    println!("  edit [list] - Edit a list name");
+    println!("  edit [list] [name] - Edit a list name");
     println!("  edit [list] [item] [content] - Edit an item in a list");
+    println!("  edit [list] [item] [status] - Edit an item status in a list");
     println!("  get [list] [item] - Get an item from a list");
     println!("  help - Show this help message");
 }
@@ -22,7 +23,8 @@ pub fn add (manager: &mut manager::Manager, name: &str) {
 }
 
 pub fn remove (manager: &mut manager::Manager, name: &str) {
-    manager.remove_list(name);
+    let index = manager.lists.iter().position(|list| list.name == name).unwrap();
+    manager.remove_list(index);
 }
 
 pub fn list (manager: &manager::Manager) -> Vec<&str> {
@@ -43,15 +45,19 @@ pub fn add_item (manager: &mut manager::Manager, list_name: &str, item_content: 
 }
 
 pub fn remove_item (manager: &mut manager::Manager, list_name: &str, item_content: &str) {
-    manager.remove_item(list_name, item_content);
+    let list = manager.get_lists().iter().find(|list| list.name == list_name).unwrap();
+    let index = list.items.iter().position(|item| item.content == item_content).unwrap();
+    manager.remove_item(list_name, index);
 }
 
 pub fn edit_item (manager: &mut manager::Manager, list_name: &str, item_content: &str, new_content: &str) {
-    manager.edit_item(list_name, item_content, new_content);
+    let list = manager.get_lists().iter().find(|list| list.name == list_name).unwrap();
+    let index = list.items.iter().position(|item| item.content == item_content).unwrap();
+    manager.edit_item(index, new_content, list_name);
 }
 
 pub fn get_item<'a> (manager: &'a manager::Manager, list_name: &str, item_content: &str) -> Option<&'a str> {
-    let list = manager.get_list(list_name).unwrap();
+    let list = manager.get_lists().iter().find(|list| list.name == list_name).unwrap();
     let item = list.items.iter().find(|item| item.content == item_content).unwrap();
     Some(&item.content)
 }
