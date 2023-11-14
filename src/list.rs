44dfb7629d17798795258cf2b07bc7fb17fb8669
datsfilipe <datsfilipe.foss@proton.me>
2023-11-14 12:37:02 -0300

    feat: add list and item impl methods

--- src/list.rs
+++ src/list.rs
@@ -1,17 +1,47 @@
 use std::time::SystemTime;
 
+#[derive(PartialEq)]
+pub enum Status {
+    Todo,
+    Doing,
+    Done,
+}
+
 pub struct Item {
-    _content: String,
-    _created_at: SystemTime,
+    pub content: String,
+    pub created_at: SystemTime,
+    pub status: Status,
 }
 
 pub struct List {
-    _items: Vec<Item>,
-    _name: String,
+    pub items: Vec<Item>,
+    pub name: String,
 }
 
 impl List {
-    pub fn _new(name: String, items: Vec<Item>) -> List {
-        List { _name: name, _items: items }
+    pub fn new(name: String, items: Vec<Item>) -> List {
+        List { name, items }
+    }
+
+    pub fn get_list(&self) -> &Vec<Item> {
+        &self.items
+    }
+
+    pub fn get_list_by_status(&self, status: Status) -> Vec<&Item> {
+        self.items.iter().filter(|item| item.status == status).collect()
+    }
+}
+
+impl Item {
+    pub fn new(content: String, created_at: SystemTime, status: Status) -> Item {
+        Item {
+            content,
+            created_at,
+            status,
+        }
+    }
+
+    pub fn get_content(&self) -> &String {
+        &self.content
     }
 }
