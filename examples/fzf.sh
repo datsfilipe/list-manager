65b881cda2661505dc3a8c8a05620a2ba8ba99b5
datsfilipe <datsfilipe.foss@proton.me>
2023-11-15 13:15:01 -0300

    refactor: update fzf example

--- examples/fzf.sh
+++ examples/fzf.sh
@@ -1,97 +1,50 @@
 #!/bin/sh
 
 list_lists() {
-  options=($(./target/debug/lm list) "[add]" "[remove]")
-  selected_option=$(printf '%s\n' "${options[@]}" | fzf --prompt="select an option: ")
+  del_cmd="list-manager remove {}"
+  add_cmd="read 'REPLY?name: ' && echo \$REPLY | list-manager add \$REPLY"
+  selected_option=$(list-manager list | fzf \
+    --bind "ctrl-o:execute($del_cmd)" \
+    --bind="ctrl-n:execute($add_cmd)")
 
   if [ -n "$selected_option" ]; then
-    case "$selected_option" in
-      "[add]")
-        add_list
-        ;;
-      "[remove]")
-        delete_list
-        ;;
-      *)
-        list_items "$selected_option"
-        ;;
-    esac
-  fi
-}
-
-delete_list() {
-  list_name=$(./target/debug/lm list | fzf --prompt="select a list to delete: ")
-  if [ -n "$list_name" ]; then
-    remove_list "$list_name"
-  fi
-}
-
-remove_list() {
-  list_name="$1"
-  ./target/debug/lm remove "$list_name"
-}
-
-add_list() {
-  new_list=$(read -p "[enter a name for the list]: " && echo "$REPLY")
-  if [ -n "$new_list" ]; then
-    ./target/debug/lm add "$new_list"
-    list_items "$new_list"
+    list_items "$selected_option"
   fi
 }
 
 list_items() {
   list_name="$1"
+  del_cmd="list-manager remove $list_name {}"
+  add_cmd="read 'REPLY?content: ' && echo \$REPLY | list-manager add $list_name \$REPLY"
   while true; do
-    options=($(./target/debug/lm list "$list_name") "[remove]" "[add]" "[back]")
-    selected_option=$(printf '%s\n' "${options[@]}" | fzf --prompt="select an option: ")
+    options=("[back]" $(list-manager list "$list_name"))
+    selected_option=$(printf '%s\n' "${options[@]}" | fzf \
+      --bind "ctrl-o:execute($del_cmd)" \
+      --bind="ctrl-n:execute($add_cmd)")
 
     if [ -z "$selected_option" ]; then
       break
-    elif [ "$selected_option" == "[add]" ]; then
-      add_item "$list_name"
-    elif [ "$selected_option" == "[remove]" ]; then
-      delete_item "$list_name"
     elif [ "$selected_option" == "[back]" ]; then
       list_lists && break
     else
-      edit_item "$list_name" "$selected_option"
+      open "$selected_option" "$list_name" && break
     fi
   done
 }
 
-delete_item() {
-  list_name="$1"
-  item_content=$(./target/debug/lm list "$list_name" | fzf --prompt="select an item to delete: ")
-  if [ -n "$item_content" ]; then
-    remove_item "$list_name" "$item_content"
-  fi
-}
-
-add_item() {
-  list_name="$1"
-  new_item=$(read -p "[enter a name for the item]: " && echo "$REPLY")
-  if [ -n "$new_item" ]; then
-    ./target/debug/lm add "$list_name" "$new_item"
-    list_items "$list_name"
-  fi
-}
-
-remove_item() {
-  list_name="$1"
-  item_content="$2"
-  ./target/debug/lm remove "$list_name" "$item_content"
-}
-
-edit_item() {
-  list_name="$1"
-  item_content="$2"
+open() {
+  item_content="$1"
+  list_name="$2"
   if [[ $item_content == *http*://* || $item_content == *https*://* ]]; then
     xdg-open "$item_content"
-    edit_item "$list_name" "$item_content" "done"
   else
-    new_status=$(printf "todo\ndoing\ndone" | fzf --prompt="select a status for the item: ")
-    if [ -n "$new_status" ]; then
-      ./target/debug/lm edit "$list_name" "$item_content" "$new_status"
+    read -p "copy to clipboard/edit [c/e]: " -n 1 -r
+    echo ""
+    if [[ $REPLY =~ ^[Ee]$ ]]; then
+      read -p "content/status: " -r
+      list-manager edit "$list_name" "$item_content" "$REPLY"
+    else
+      echo "$item_content" | wl-copy
     fi
   fi
 }
